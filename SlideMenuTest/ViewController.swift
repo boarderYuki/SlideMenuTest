7e445e254c29ec6c4bc19e3b6d86e4292331ceb0
yuki.pro <yuki.kr@gmail.com>
2017-06-28 09:57:00 +0900

    SlideMenuTest

--- SlideMenuTest/ViewController.swift
+++ SlideMenuTest/ViewController.swift
@@ -7,13 +7,6 @@
 //
 
 
-// 슬라이드 오픈시에 보여질 메뉴를 만든후에
-// view 1 화면으로 전체를 덮고
-// 슬라이드 메뉴 클릭시 view 1 화면의 좌우 제약조건을 조절하여
-// 밑에 가려져있던 사이드 메뉴를 보여지게 한다.
-// 참고: https://www.youtube.com/watch?v=SGkYb820zr4
-
-
 import UIKit
 
 class ViewController: UIViewController {
